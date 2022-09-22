e77c1dd733c7657840eb1bb787078fe7793b3e2c
EricChukoltyansky <kazohelmet@gmail.com>
2022-09-22 12:04:32 +0300

    swatch function build up

--- multi select/src/App.tsx
+++ multi select/src/App.tsx
@@ -1,41 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [appRenderIndex, setAppRenderIndex] = useState(0);
+  console.log(`App render ${appRenderIndex} times`);
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
+        App render
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
-
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/EricChukoltyansky/useMemo.git
-// git push -u origin main
+export default App;
